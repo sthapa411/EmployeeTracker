64e73d5f7d45ebbebe23b78d8cc666f8eab4d14c
sthapa <san20hal@gmail.com>
2020-07-23 18:40:41 -0400

    main questions prompt command line added

--- index.js
+++ index.js
@@ -14,3 +14,50 @@ connection.connect(function(err) {
   if (err) throw err;
   askQuestions();
 });
+
+unction askQuestions() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Departments",
+          "View All Roles",
+          "Add Employees",
+          "Add Departments",
+          "Add Roles",
+          "Update Employee Role",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+          case "View All Employees":
+            empAllSearch();
+            break;
+          case "View All Departments":
+            deptSearch();
+            break;
+          case "View All Roles":
+            roleSearch();
+            break;
+          case "Add Employees":
+            addEmp();
+            break;
+          case "Add Departments":
+            addDept();
+            break;
+          case "Add Roles":
+            addRole();
+            break;
+          case "Update Employee Roles":
+            updateEmpRole();
+            break;
+          case "exit":
+            connection.end();
+            break;
+        }
+      });
+  };
