5b36ad2864662007369a62cef77ff2c710e5450c
sthapa <san20hal@gmail.com>
2020-07-23 18:29:51 -0400

    INSERT INTO employee table

--- schema.sql
+++ schema.sql
@@ -7,6 +7,27 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NOT NULL, 
+  manager_id INT, 
   PRIMARY KEY (employee_id)
 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 01);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 2, 01);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Spenser", "Ole", 3, 02);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Zina", "Scott", 4, 03);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Robert", "Lee", 5, 03);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sam", "Skite", 6, 02);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Navin", "Kumar", 7, 03);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Rita", "Duch", 8, 01);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Trista", "Dane", 5, 03);
+INSERT INTO employee  (first_name, last_name, role_id, manager_id)
+VALUES ("Rupert", "lee", 6, 02);
